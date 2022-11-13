51de12107992a14c1039f94e4d04e610d4af886e
erik <110602802+erikidelchik@users.noreply.github.com>
2022-11-13 20:30:24 +0200

    Update main.c

--- main.c
+++ main.c
@@ -10,34 +10,34 @@ int main(){
 	printf("enter second number ");
 	scanf("%d",&y);
 	
-	printf("the strong numbers are: ");
+	printf("the strong numbers are:");
 	
 	for(int i=x;i<=y;i++){
-	       if(isStrong(i)==1) printf("%d ",i);
+	       if(isStrong(i)==1) printf(" %d ",i);
 	}
 	printf("\n");
 	
 	
-	printf("the ArmStrong numbers are: ");
+	printf("the ArmStrong numbers are:");
 	
 	for(int i=x;i<=y;i++){
-	       if(isArmstrong(i)==1) printf("%d ",i);
+	       if(isArmstrong(i)==1) printf(" %d ",i);
 	}
 	printf("\n");
 	
 	
-	printf("the Palindrome numbers are: ");
+	printf("the Palindrome numbers are:");
 	
 	for(int i=x;i<=y;i++){
-	       if(isPalindrome(i)==1) printf("%d ",i);
+	       if(isPalindrome(i)==1) printf(" %d ",i);
 	}
 	printf("\n");
 	
 	
-	printf("the Prime numbers are: ");
+	printf("the Prime numbers are:");
 	
 	for(int i=x;i<=y;i++){
-	       if(isPrime(i)==1) printf("%d ",i);
+	       if(isPrime(i)==1) printf(" %d ",i);
 	}
 	printf("\n");
 
