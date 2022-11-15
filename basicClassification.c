3144a829f2c0b236a31774cd69f8afe1afb097eb
erik <110602802+erikidelchik@users.noreply.github.com>
2022-11-15 13:35:42 +0200

    Update basicClassification.c

--- basicClassification.c
+++ basicClassification.c
@@ -5,7 +5,8 @@ int isPrime(int);
 int isStrong(int);
 
 int isPrime(int x){
-    if(x==1) return 1;
+	
+    if(x==1) return 1;    
     if (x==0) return 0;
     for(int i=2;i<x;i++){
         if(x%i==0) return 0;
@@ -15,24 +16,17 @@ int isPrime(int x){
 
 int isStrong(int x){
     int n = x;
-    int c = 0;
-    while(n!=0){
-        n=n/10;
-        c++;
-    }
-
-    int a[c];
-    int b = x;
-    for(int i=0;i<c;i++){
-        a[i] = b%10;
-        b = b/10;
-    }
+    int c = 0, d=0;
     int sum = 0;
-    for(int i=0;i<c;i++){
-        sum+= factorial(a[i]);
+    while(n!=0){
+    	d = n%10;
+    	c = factorial(d);
+    	sum+=c;
+        n=n/10;
     }
     if(sum==x) return 1;
     return 0;
+    
 }
 
 int factorial(int x){
